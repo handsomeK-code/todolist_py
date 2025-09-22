507ed1d507bdac07e57cc749a873c453371bda75
handsomeK <ckyu388@gmail.com>
2025-09-22 19:26:02 +0800

    showList base finish

--- show_list.py
+++ show_list.py
@@ -1,2 +1,8 @@
 def showList (tasks) -> dict:
-    
+    #check tasks empty or not
+    if not tasks:
+        return print('you dont have any task right now! please make one!')
+    choose_progress =input("Filter: ")
+    #todo: add check
+    if choose_progress== 1:
+        
