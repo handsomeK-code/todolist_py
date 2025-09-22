2a3bf915211d022b7dcc59cb7232dbcc376fc18a
handsomeK <ckyu388@gmail.com>
2025-09-22 15:09:32 +0800

    edit function added

--- edit_list.py
+++ edit_list.py
@@ -3,12 +3,15 @@ def editTask (tasks:list) -> list:
         return print("you have no todo task right now! Please add a new list")
     print((task['description'] for task in tasks ))
     task_to_edit = input("please the fullname of the task you want to edit")
-    #add checks to prevent wrong input 
+    #todo:add checks to prevent wrong input 
     prop_to_edit = input("what do you want to edit")
     #change task name
-    if prop_to_edit == 0:
+    if prop_to_edit == 1:
         name = input('Please input the new task name you want to change to: ')
         tasks[task_to_edit]['description'] = name
         tasks[name] = tasks.pop(task_to_edit)
         return tasks
-    elif
+    #del task 
+    elif prop_to_edit == 2:
+        tasks.pop(task_to_edit)
+        return tasks
