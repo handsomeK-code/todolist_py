ea4519167fe6a1a39c51e8521054edd5fcd4f58d
handsomeK <ckyu388@gmail.com>
2025-09-30 09:12:19 +0900

    base created

--- show_list.py
+++ show_list.py
@@ -4,5 +4,12 @@ def showList (tasks) -> dict:
         return print('you dont have any task right now! please make one!')
     choose_progress =input("Filter: ")
     #todo: add check
+    #show not done
     if choose_progress== 1:
-        
+        print((not_done for not_done in tasks ))
+    #show in-progress
+    if choose_progress == 2:
+        print((in_progress for in_progress in tasks))
+    #show done
+    if choose_progress == 3:
+        print((done for done in tasks))
